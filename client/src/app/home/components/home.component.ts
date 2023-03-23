c880daf568f42d68411f47f31af3342edffb3607
SeCre827 <lefterisoikonomy@yahoo.gr>
2023-03-24 01:27:16 +0200

    added home page

---
+++ client/src/app/home/components/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth/services/auth.service';
+
+@Component({
+  selector: 'home',
+  templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  isLoggedInSub: Subscription | undefined;
+  constructor(private authService: AuthService, private router: Router) {}
+  ngOnInit(): void {
+    this.isLoggedInSub = this.authService.isLoggenIn$.subscribe(
+      (isLoggenIn) => {
+        if (!isLoggenIn) {
+          this.router.navigateByUrl('/boards');
+        }
+      }
+    );
+  }
+  ngOnDestroy(): void {
+    if (this.isLoggedInSub) {
+      this.isLoggedInSub.unsubscribe();
+    }
+  }
+}
